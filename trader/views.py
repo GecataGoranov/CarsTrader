85a3f84f6f59820d50e9f719c3a47c62d25bc8c8
GecataGoranov <gogogoranov04@gmail.com>
2023-04-12 18:05:33 +0300

    Started adding the boilerplate files and building the navbar

--- trader/views.py
+++ trader/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import TemplateView
 
 
 # Create your views here.
 
-class IndexView(View):
-    pass
+class IndexView(TemplateView):
+    template_name = "trader/index.html"
+    
