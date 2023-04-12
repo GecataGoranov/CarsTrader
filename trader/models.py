3728a047ef51f46a10c2f1960eec177f5af75357
GecataGoranov <gogogoranov04@gmail.com>
2023-04-12 19:27:23 +0300

    Completed the Navbar (for now) and began working on the models

--- trader/models.py
+++ trader/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+class EngineType(models.Choices):
+    PETROL = "P", "Petrol"
+    DIESEL = "D", "Diesel"
+    ELECTRIC = "E", "Electric"
+    HYBRID = "H", "Hybrid"
+    PLUG_IN_HYBRID = "PH", "Plug-in Hybrid"# Create your models here.
+
+
+class Category(models.Model):
+    pass
+
+
+class Manufacturer(models.Model):
+    pass
+
+
+class Car(models.Model):
+    engine_types = [
+        ("P", "Petrol"),
+        ("D", "Diesel"),
+        ("E", "Electric"),
+        ("H", "Hybrid"),
+        ("I", "Plug-in Hybrid")
+    ]
+
+    gearbox_types = [
+        ("M", "manual"),
+        ("A", "automatic"),
+        ("S", "semi-automatic")
+    ]
+
+    manufacturer = models.ForeignKey(Manufacturer, related_name="manufacturer", on_delete=models.CASCADE)
+    model = models.CharField(max_length=30)
+    category = models.ForeignKey(Category, related_name="category", on_delete=models.CASCADE)
+    engine_type = models.CharField(max_length=1, choices=engine_types)
+    gearbox_type = models.CharField(max_length=1, choices=gearbox_types)
+    power = models.IntegerField(name="power", max_length=2000)
+    eurostandard = models.IntegerField(name="eurostandard", max_length=6)
+
+
+
--- trader/views.py
+++ trader/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 
 # Create your views here.
 
 class IndexView(TemplateView):
     template_name = "trader/index.html"
-    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["page"] = "home"
+        return context
